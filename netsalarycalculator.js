7a73b54e5da42458c9a4981208ff045a4ccc5901
VERA-OBIERO <vera.obiero@gmail.com>
2023-09-24 16:18:24 +0300

    Final Net Salary Calculator

--- netsalarycalculator.js
+++ netsalarycalculator.js
@@ -1,29 +1,43 @@
 //Net Salary Calculator Function
   function netSalaryCalculator(){
-    // Prompt the user to enter basic salary and preserve decimal numbers if any
-  var basicSalary = prompt("Please Enter your basic salary: ");
-  var marks = parseFloat(basicSalary);
+   // Prompt the user to enter basic salary and preserve decimal numbers if any
+   var salary = prompt("Please Enter your basic salary: ");
+   var basicSalary = parseFloat(salary);
 
-  //NSSF Calcultor based on basic salary so as to get taxable pay
+   // Prompt the user to enter total amount of allowances and preserve decimal numbers if any
+   var allowance = prompt("Enter total amount of allowances: ");
+   var totalAllowances = parseFloat(allowance);
+
+   // Prompt the user to enter total amount of retirement benefits excluding NSSF
+   // and preserve decimal numbers if any
+   var benefit = prompt("Enter total amount of retirement/pension contributions excluding NSSF: ");
+   var contributionBenefit = parseFloat(benefit);
+
+    //Calculate Gross Salary
+    var grossSalary;
+    grossSalary = basicSalary + totalAllowances;
+
+  //NSSF Calculator based on gross salary so as to get taxable pay
     var NSSF;
-    if (basicSalary >= 18000){
+    if (grossSalary >= 18000){
         NSSF = 1080;
-    }else if (basicSalary >= 14000){
+    }else if (grossSalary >= 14000){
         NSSF = 840;
-    }else if (basicSalary >= 10000){
+    }else if (grossSalary >= 10000){
         NSSF = 600;
-    }else if (basicSalary >= 6000){
+    }else if (grossSalary >= 6000){
         NSSF = 360;
-    }else if (basicSalary >= 4500){
+    }else if (grossSalary >= 4500){
         NSSF = 270;
-    }else if (basicSalary >= 3000){
+    }else if (grossSalary >= 3000){
         NSSF = 180;
     }else {
         NSSF = 0;
     }
+    
     //Calculate Taxable Pay
     var taxablePay;
-    taxablePay = basicSalary - NSSF;
+    taxablePay = grossSalary - (NSSF + contributionBenefit);
 
     //PAYE Calculator
     if (taxablePay <= 24000){
@@ -38,44 +52,44 @@
         PAYE = (0.1*24000) + (0.25*8333) + (0.3*467667) + (0.325*800000) + (0.35*(taxablePay-800000));
     }
 
-    //NHIF Calculator based on basic salary
+    //NHIF Calculator based on gross salary
     var NHIF;
-    if (basicSalary > 99999){
+    if (grossSalary > 99999){
         NHIF = 1700;
-    }else if (basicSalary > 89999){
+    }else if (grossSalary > 89999){
         NHIF = 1600;
-    }else if (basicSalary > 79999){
+    }else if (grossSalary > 79999){
         NHIF = 1500;
-    }else if (basicSalary > 69999){
+    }else if (grossSalary > 69999){
         NHIF = 1400;
-    }else if (basicSalary > 59999){
+    }else if (grossSalary > 59999){
         NHIF = 1300;
-    }else if (basicSalary > 49999){
+    }else if (grossSalary > 49999){
         NHIF = 1200;
-    }else if (basicSalary > 44999){
+    }else if (grossSalary > 44999){
         NHIF = 1100;
-    }else if (basicSalary > 39999){
+    }else if (grossSalary > 39999){
         NHIF = 1000;
-    }else if (basicSalary > 34999){
+    }else if (grossSalary > 34999){
         NHIF = 950;
-    }else if (basicSalary > 29999){
+    }else if (grossSalary > 29999){
         NHIF = 900;
-    }else if (basicSalary > 24999){
+    }else if (grossSalary > 24999){
         NHIF = 850;
-    }else if (basicSalary > 19999){
+    }else if (grossSalary > 19999){
         NHIF = 750;
-    }else if (basicSalary > 14999){
+    }else if (grossSalary > 14999){
         NHIF = 600;
-    }else if (basicSalary > 11999){
+    }else if (grossSalary > 11999){
         NHIF = 500;
-    }else if (basicSalary > 7999){
+    }else if (grossSalary > 7999){
         NHIF = 400;
-    }else if (basicSalary > 5999){
+    }else if (grossSalary > 5999){
         NHIF = 300;
     }else {
         NHIF = 150;
     }
-    //calculate Tax relief on PAYE which is 15% of insurance relief(including NHIF) up to 5000
+    //calculate relief on NHIF
     var relief;
     relief = 0.15 * NHIF;
 
@@ -84,9 +98,9 @@
     var newPAYE;
     newPAYE = PAYE - (personalRelief + relief)
     
-    //Housing Levy Calculator based on basic salary
+    //Housing Levy Calculator based on gross salary
     var housingLevy;
-    housingLevy = 0.015 * basicSalary;
+    housingLevy = 0.015 * grossSalary;
 
     //calculate deductions
     var deductions;
@@ -94,10 +108,10 @@
 
     //calculate Net Salary
     var netSalary;
-    netSalary = basicSalary - deductions;
+    netSalary = taxablePay - deductions;
 
     //Display the net Salary
-    console.log(`Your Gross Salary is ${basicSalary}\n
+    console.log(`Your Gross Salary is ${grossSalary}\n
     Deductions: PAYE = ${newPAYE}, NHIF = ${NHIF}, NSSF = ${NSSF}, Housing Levy = ${housingLevy}\n
     The Net Salary is ${netSalary}`)
  }
